a6d451c0344d1bd3b334625696c5d4f8598095cc
renderstring <glebovilya@bk.ru>
2024-06-09 10:32:07 +0300

    Calculator

---
+++ Calc/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите выражение: ");
+        String expression = scan.nextLine();
+
+        String result = calc(expression);
+        System.out.println("Результат: " + result);
+
+    }
+
+    public static String calc(String input) {
+
+        String result;
+
+        String[] elements = input.split(" ");
+
+        try {
+            int el1 = Integer.parseInt(elements[0]);
+            int el2 = Integer.parseInt(elements[2]);
+            char operation = elements[1].charAt(0);
+
+            if (elements.length != 3) {
+                throw new RuntimeException("Неправильное количество элементов");
+            }
+
+            if ((el1 < 1 || el1 > 10) || (el2 < 1 || el2 > 10)) {
+                throw new RuntimeException("Нельзя использовать числа меньше 1 и больше 10");
+            }
+
+            result = switch (operation) {
+                case '+' -> String.valueOf(el1 + el2);
+                case '-' -> String.valueOf(el1 - el2);
+                case '*' -> String.valueOf(el1 * el2);
+                case '/' -> String.valueOf(el1 / el2);
+                default -> throw new RuntimeException("Неправильная операция");
+            };
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Неправильный формат");
+        }
+
+        return result;
+    }
+}
